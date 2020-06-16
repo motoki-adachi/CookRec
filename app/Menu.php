ec0f2d13cd9b149c10ad9f63863204dda0ef3e36
Motoki Adachi <zyx.wvu43@icloud.com>
2020-06-16 22:12:18 +0900

    create_project

---
+++ app/Menu.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'image',
+        'user_id',
+        'point',
+        'upbringing'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
