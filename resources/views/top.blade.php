dc47e740664b46ec6933b0bee7b80c880b17a611
Motoki Adachi <zyx.wvu43@icloud.com>
2020-06-17 20:57:03 +0900

    create_top-wrapper

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="top-wrapper">
+
+  <div class="top-wrapper__left">
+    <div class="top-wrapper__left--logo">CookRec</div>
+  </div>
+
+  <div class="top-wrapper__center">
+    <div class="top-wrapper__center--search">
+      <form action="#">
+        <input type="text" class="recipe-search" placeholder="調べたいレシピを検索">
+        <input type="submit" class="serch-submit" value="検索">
+      </form>
+    </div>
+    <div class="top-wrapper__center--content">
+      人気検索ワードが出てくる
+    </div>
+  </div>
+
+  <div class="top-wrapper__right">
+    <div class="top-wrapper__right--dir">
+      <a href="#">保存したレシピ一覧に飛ぶ</a>
+    </div>
+    <div class="top-wrapper__right--create">
+      <a href="#">レシピ作成画面に飛ぶ</a>
+    </div>
+  </div>
+
+</div>
 @endsection
